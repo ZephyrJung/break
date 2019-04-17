31d3f088f7c58bf67c74d6c4bc1026f4a647b07d
张煜 <zephyrjung@126.com>
2019-04-17 14:41:35 +0800

    temporay background and time tick on window

--- renderer.js
+++ renderer.js
@@ -1,3 +1,17 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+const settings = require("./settings");
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+const count = document.getElementById("count");
+
+title.innerText = settings.messages[0].title;
+content.innerText = settings.messages[0].content;
+var time = 30;
+setInterval(function () {
+    time = time - 1;
+    count.innerText = time;
+    if (time == 0) {
+    }
+}, 1000);
---
+++ settings.js
@@ -0,0 +1,65 @@
+module.exports = {
+    messages: [
+        {
+            title: "Not alone",
+            content: "Do you find it hard to take a break alone? Try to do it with a co-worker. Aside from making it easier to stick to breaks, you will have a chance to get to know them better. Taking breaks together increases productivity."
+        },
+        {
+            title: "Step away",
+            content: "Do you ever notice how your brain can figure things out by itself? All it takes is to step away from the computer and take a break to think about something totally unrelated."
+        },
+        {
+            title: "Microbreaks",
+            content: "Rest is a key component in ensuring the performance of the musculoskeletal system. Frequent breaks can decrease the duration of a task and help lower the exposure to ergonomic injury risk."
+        },
+        {
+            title: "Meditation",
+            content: "Research studies suggest that mindfulness-based exercises help decrease anxiety, depression, stress, and pain, and help improve general health, mental health, and quality of life. Not sure how to start? There are numerous apps to help you out."
+        },
+        {
+            title: "Blink",
+            content: "Looking at screens for a long time causes you to blink less, thus exposing your eyes to the air. Blink rapidly for a few seconds to refresh the tear film and clear dust from the eye surface."
+        },
+        {
+            title: "Ergonomics",
+            content: "Improper height and angle of the keyboard, mouse, monitor or working surface can cause health problems. Take some time to read about desk ergonomics."
+        },
+        {
+            title: "Move",
+            content: "There are a lot of ways you can exercise within your office. Try marching in place or doing desk push-ups."
+        },
+        {
+            title: "Change",
+            content: "Do you have a stability ball or standing work desk? Consider replacing your desk chair with them for a while."
+        },
+        {
+            title: "Notice",
+            content: "Are you daydreaming or having trouble focusing? It is a sign that you need to take a break."
+        },
+        {title: "Tech", content: "How about taking a no-tech walk?"},
+        {
+            title: "Metabolism",
+            content: "Emerging research shows that sitting for long periods of time contributes to risk of metabolic syndrome, heart attack and stroke risk and overall death risk, among others. Taking regular walking breaks can help your circulation, working to counteract some of those problems."
+        },
+        {
+            title: "Active Meetings",
+            content: "How about moving meetings from the conference room to the concourse? Walking not only burns calories but it may even foster a sense of collaboration."
+        },
+        {
+            title: "Fruit",
+            content: "Take your time and eat some fruit. Slowly. Notice the flavor, the texture, the freshness."
+        },
+        {
+            title: "Bathrooms",
+            content: "Walk to the farthest bathroom in the worksite facility when going to the restroom."
+        },
+        {
+            title: "Coffee break",
+            content: "Going on coffee break? Consider doing a 5-minute walk every time you go for one."
+        },
+        {
+            title: "Colleagues",
+            content: "Do not email or message office colleagues, walk to their desks to communicate with them."
+        }
+    ]
+}
